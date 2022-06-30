4a574f12205f0a1d2739355403220b2cdd68d700
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2022-06-30 13:36:30 +0100

    finishing the UI for the Rock paper Scissors game

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const btnR = document.getElementById('btnR');
+const btnP = document.getElementById('btnP');
+const btnS = document.getElementById('btnS');
+const btns = document.querySelectorAll('.btn');
+const main = document.getElementById('main');
+const display = document.getElementById('display');
+const playerCounter = document.getElementById('counterP');
+const computerCounter = document.getElementById('counterC');
+let playerS;
+let playerScore = 0;
+let computerScore = 0;
+
 const computerPlay = () => {
     let randomNum = Math.floor(Math.random() * 3) + 1;
     let result;
@@ -13,54 +25,79 @@ const computerPlay = () => {
     return result;
 };
 const playRound = (pleyerSelection, computerSelection) => {
-    let  playerS = pleyerSelection.toLowerCase();
+    let  playerS = pleyerSelection;
     let computerS = computerSelection;
     let result; 
     if (playerS == computerS) {
-        console.log(`you Tie! ${computerS} same as ${playerS}.`);
+        display.innerHTML = `you Tie! ${computerS} same as ${playerS}.`;
         result = 0;
     }
     else if (playerS == "rock" && computerS == "paper") {
-        console.log(`you Lose! ${computerS} beats ${playerS}.`);
+        computerScore += 1;
+        display.innerHTML = `you Lose! ${computerS} beats ${playerS}.`;
+        playerCounter.innerHTML = playerScore;
+        computerCounter.innerHTML = computerScore;
         result = -1;
     }
     else if (playerS == "paper" && computerS == "scissors") {
-        console.log(`you Lose! ${computerS} beats ${playerS}.`);
+        computerScore += 1;
+        display.innerHTML = `you Lose! ${computerS} beats ${playerS}.`;
+        playerCounter.innerHTML = playerScore;
+        computerCounter.innerHTML = computerScore;
         result = -1;
     }
     else if (playerS == "scissors" && computerS == "rock") {
-        console.log(`you Lose! ${computerS} beats ${playerS}.`);
+        computerScore += 1;
+        display.innerHTML = `you Lose! ${computerS} beats ${playerS}.`;
+        playerCounter.innerHTML = playerScore;
+        computerCounter.innerHTML = computerScore;
         result = -1;
     }
     else{
-        console.log(`you Win! ${playerS} beats ${computerS}.`);
+        playerScore += 1;
+        display.innerHTML = `you Win! ${playerS} beats ${computerS}.`;
+        playerCounter.innerHTML = playerScore;
+        computerCounter.innerHTML = computerScore;
         result = 1;
     }
     return result;
 };
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let winner;
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Enter your play(rock,paper or scissors) :");
-        let computerSelection = computerPlay();
-        let roundResult = playRound(playerSelection, computerSelection);
-        if (roundResult == 1) {
-            playerScore += 1;
+
+const game = (pS) => {
+    let computerSelection = computerPlay();
+    playRound(pS, computerSelection);
+    
+    if (playerScore >= 5 || computerScore >= 5) {
+        if (playerScore >= 5) {
+            main.innerHTML = `<div style="border: 2px solid black;border-radius: 8px;margin-top:100px;">
+            <h1>Winner Winner Chicken Dinner ;)</h1><br>
+            <h2>Congrats you have beat the Computer</h2>
+            </div>`;
         }
-        if (roundResult == -1) {
-            computerScore += 1;
+        else if (playerScore <= 5) {
+            main.innerHTML = `<div style="border: 2px solid black;border-radius: 8px;margin-top:100px;">
+            <h1>You allways have been a LOSER!</h1><br>
+            <h2>A computer beat your dump ass</h2>
+            </div>`;
         }
-        console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}.`);
+    }else{
+        return;
     }
-    if (playerScore > computerScore) {
-        console.log("Winner Winner Chicken Dinner ;)");
-    }
-    else if (playerScore < computerScore) {
-        console.log("You allways have been a LOSER!");
-    }else {
-        console.log("Tie ¯\\_(ツ)_/¯" );
-    }
+    
 };
-game();
+
+btnR.addEventListener('click', function(e){
+    playerS = e.target.value;
+    game(playerS);
+});
+
+btnP.addEventListener('click', function(e){
+    playerS = e.target.value;
+    game(playerS);
+});
+
+btnS.addEventListener('click', function(e){
+    playerS = e.target.value;
+    game(playerS);
+});
+
